b6a961381ac40822fe9e1f1fab29423102727df4
Felipe Trova <felipee.trova@gmail.com>
2020-05-14 14:19:32 -0300

    Create a loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Loading } from './LoadingStyle';
+
+export default function LoadingComponent() {
+  return (
+    <Loading>
+      <div className="loading-overlay is-active is-full-page">
+        <div className="loading-background"></div>
+        <div className="loading-icon"></div>
+      </div>
+    </Loading>
+  );
+}
